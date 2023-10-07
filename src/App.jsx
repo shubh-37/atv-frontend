beeee633b41becb9f1ac9c79efce736093c5ef12
shubh-37 <shubh.arya37@gmail.com>
2023-10-07 22:30:23 +0530

    added new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 function App() {
-
   return (
     <>
-
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
