d02a22e13df76a03696fcda8cf015f53c4725832
shubh-37 <shubh.arya37@gmail.com>
2023-10-08 00:46:39 +0530

    wrapping app with product context

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import AuthContextProvider from "./context/AuthContextProvider.jsx";
+import ProductContextProvider from "./context/ProductContextProvider.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <AuthContextProvider>
-        <App />
+        <ProductContextProvider>
+          <App />
+        </ProductContextProvider>
       </AuthContextProvider>
     </Router>
   </React.StrictMode>
