c84781c6bb971cc23a0586a681ad6cc12d0ff466
Saras Arya <saras@anattadesign.com>
2023-10-08 23:04:14 +0530

    in works commit

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -5,85 +5,73 @@ const Scanner = (props) => {
   // eslint-disable-next-line react/prop-types
   const { onDetected } = props;
 
-  const config = {
-    inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      constraints: {
-        width: { max: 300 },
-        height: { max: 300 },
-        facingMode: "environment",
-        aspectRatio: { min: 1, max: 2 },
-      },
-    },
-    locator: {
-      patchSize: "medium",
-      halfSample: true,
-    },
-    numOfWorkers: window.navigator.hardwareConcurrency,
-    frequency: 10,
-    decoder: {
-      readers: ["code_93_reader"],
-    },
-    locate: true,
-  };
+  // const config = {
+  //   inputStream: {
+  //     name: "Live",
+  //     type: "LiveStream",
+  //     constraints: {
+  //       width: { max: 300 },
+  //       height: { max: 300 },
+  //       facingMode: "environment",
+  //       aspectRatio: { min: 1, max: 2 },
+  //     },
+  //   },
+  //   locator: {
+  //     patchSize: "medium",
+  //     halfSample: true,
+  //   },
+  //   numOfWorkers: window.navigator.hardwareConcurrency,
+  //   frequency: 10,
+  //   decoder: {
+  //     readers: ["code_93_reader"],
+  //   },
+  //   locate: true,
+  // };
 
   useEffect(() => {
-    Quagga.init(config, (err) => {
-      if (err) {
-        console.log(err, "error msg");
-      }
-      Quagga.start();
-      return () => {
-        Quagga.stop();
-      };
-    });
-    console.log("inside useEff");
-    //detecting boxes on stream
-    Quagga.onProcessed((result) => {
-      console.log("inside processing");
-      var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-
-      if (result) {
-        if (result.boxes) {
-          drawingCtx.clearRect(
-            0,
-            0,
-            Number(drawingCanvas.getAttribute("width")),
-            Number(drawingCanvas.getAttribute("height"))
-          );
-          result.boxes
-            .filter(function (box) {
-              return box !== result.box;
-            })
-            .forEach(function (box) {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-                color: "green",
-                lineWidth: 2,
-              });
-            });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-            color: "#00F",
-            lineWidth: 2,
+    var App = {
+      _scanner: null,
+      init: function() {
+          this.attachListeners();
+      },
+      decode: function(src) {
+          Quagga
+              .decoder({readers: ['code_93_reader']})
+              .locator({patchSize: 'medium'})
+              .fromImage(src, {size: 800})
+              .toPromise()
+              .then(function(result) {
+                  document.querySelector('input.isbn').value = result.codeResult.code;
+              })
+              .catch(function() {
+                  document.querySelector('input.isbn').value = "Not Found";
+              })
+              .then(function() {
+                  this.attachListeners();
+              }.bind(this));
+      },
+      attachListeners: function() {
+          var self = this,
+              button = document.querySelector('.input-field input + .button.scan'),
+              fileInput = document.querySelector('.input-field input[type=file]');
+  
+          button.addEventListener("click", function onClick(e) {
+              e.preventDefault();
+              button.removeEventListener("click", onClick);
+              document.querySelector('.input-field input[type=file]').click();
           });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(
-            result.line,
-            { x: "x", y: "y" },
-            drawingCtx,
-            { color: "red", lineWidth: 3 }
-          );
-        }
+  
+          fileInput.addEventListener("change", function onChange(e) {
+              e.preventDefault();
+              debugger;
+              fileInput.removeEventListener("change", onChange);
+              if (e.target.files && e.target.files.length) {
+                  self.decode(URL.createObjectURL(e.target.files[0]));
+              }
+          });
       }
-    });
-    console.log("some");
-    Quagga.onDetected(detected);
+  };
+  App.init();
   }, []);
 
   const detected = (result) => {
@@ -95,6 +83,7 @@ const Scanner = (props) => {
     // QuaggaJS would look for an element that matches
     // the CSS selector #interactive.viewport
     <div id="interactive" className="viewport" />
+   
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,19 +11,26 @@ export default function Dashboard() {
     setBarcode(result);
   };
   useEffect(() => {
-    setBarcode("hey");
+    setBarcode("hey hi");
   }, []);
   return (
     <>
       <button onClick={() => setIsOpen(true)}>Upload</button>
       {isOpen && <ProductForm noChangeModal={setIsOpen} />}
       <p>{barcode.length > 0 ? barcode : "Barcode value will come here"}</p>
-      <button onClick={() => setCamera(!camera)}>
+      {/* <button onClick={() => setCamera(!camera)}>
         {camera ? "Stop scanning" : "Scan barcode"}
       </button>
       <div className="container">
         {camera && <Scanner onDetected={onDetected} />}
-      </div>
+      </div> */}
+
+    <div className="input-field">
+        <label htmlFor="isbn_input">Code 93:</label>
+        <input id="isbn_input" className="isbn" type="text" />
+        <button type="button" className="icon-barcode button scan">Scan Barcode</button>
+        <input type="file" id="file" capture/>
+    </div>
     </>
   );
 }
