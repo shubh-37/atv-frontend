ae3b1c9e751c29900fb7860dc887f22e18a20713
shubh-37 <shubh.arya37@gmail.com>
2023-10-10 22:49:22 +0530

    new implementation of barcode scanning

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Quagga from "quagga";
+
+const FileBarcodeDecoder = () => {
+  const [file, setFile] = useState(null);
+  const [barcodeOutput, setBarcodeOutput] = useState("No barcode selected");
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const decodeBarcodeFromFile = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const imageBase64 = event.target.result;
+
+        // Initialize QuaggaJS
+        Quagga.decodeSingle(
+          {
+            src: imageBase64,
+            numOfWorkers: 0,
+            decoder: { readers: ["code_93_reader"] }, // Use the Code 93 reader
+          },
+          (result) => {
+            if (result && result.codeResult) {
+              const decodedBarcode = result.codeResult.code;
+              setBarcodeOutput(decodedBarcode);
+              // Handle the decoded barcode as needed
+            } else {
+              setBarcodeOutput("No barcode found in the image.");
+            }
+          }
+        );
+      };
+
+      // Read the selected file as a data URL
+      reader.readAsDataURL(file);
+    } else {
+      console.error("No file selected.");
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <div>Decoded Barcode: {barcodeOutput}</div>
+      <button onClick={decodeBarcodeFromFile}>Decode Barcode</button>
+    </div>
+  );
+};
+
+export default FileBarcodeDecoder;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,36 +1,14 @@
-import { useEffect } from "react";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import ProductForm from "../components/ProductForm";
-import { productContext } from "../context/ProductContextProvider";
-import Scanner from "../components/Scanner";
+import FileBarcodeDecoder from "../components/FileUpload";
 export default function Dashboard() {
   const [isOpen, setIsOpen] = useState(false);
-  const [camera, setCamera] = useState(false);
-  const { barcode, setBarcode } = useContext(productContext);
-  const onDetected = (result) => {
-    setBarcode(result);
-  };
-  useEffect(() => {
-    setBarcode("hey hi");
-  }, []);
+
   return (
     <>
       <button onClick={() => setIsOpen(true)}>Upload</button>
       {isOpen && <ProductForm noChangeModal={setIsOpen} />}
-      <p>{barcode.length > 0 ? barcode : "Barcode value will come here"}</p>
-      {/* <button onClick={() => setCamera(!camera)}>
-        {camera ? "Stop scanning" : "Scan barcode"}
-      </button>
-      <div className="container">
-        {camera && <Scanner onDetected={onDetected} />}
-      </div> */}
-
-    <div className="input-field">
-        <label htmlFor="isbn_input">Code 93:</label>
-        <input id="isbn_input" className="isbn" type="text" />
-        <button type="button" className="icon-barcode button scan">Scan Barcode</button>
-        <input type="file" id="file" capture/>
-    </div>
+      <FileBarcodeDecoder />
     </>
   );
 }
