ad26ff31523549c6b23474200e136854f7567344
shubh-37 <shubh.arya37@gmail.com>
2023-10-15 13:13:20 +0530

    added new constants

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,18 @@ const SUCCESS = "success";
 const NOT_FOUND = "not found";
 const UNKNOWN = "unknown";
 const BAD_REQUEST = "bad request";
+const NO_BARCODE = "no barcode";
+const PRODUCT_FOUND = "product found";
+const CREATE_PRODUCT = "new product";
+const NO_IMAGE = "no image";
 
-export { SUCCESS, NOT_FOUND, UNKNOWN, BAD_REQUEST };
+export {
+  SUCCESS,
+  NOT_FOUND,
+  UNKNOWN,
+  BAD_REQUEST,
+  NO_BARCODE,
+  PRODUCT_FOUND,
+  CREATE_PRODUCT,
+  NO_IMAGE
+};
