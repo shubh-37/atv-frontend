78d0cd5ee09666ef62ce916a2255091c66ca218c
Shubh Arya <shubh.arya37@gmail.com>
2023-12-16 17:54:06 +0530

    added frontend authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import RequiresAuth from "./components/RequiresAuth";
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<RequiresAuth><Dashboard /></RequiresAuth>} />
       </Routes>
       <ToastContainer />
     </>
