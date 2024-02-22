9df4f7ad6f476f147ef863eff3c6d5d605991a49
Shubh Arya <shubh.arya37@gmail.com>
2024-02-22 23:05:22 +0530

    added sendotp and verifyotp apis

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,25 +1,43 @@
-import { createContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export const authContext = createContext();
-import axios from "axios";
-import { NOT_FOUND, SUCCESS, UNKNOWN } from "../constants";
-const {VITE_API_URL} = import.meta.env;
+import axios from 'axios';
+import { NOT_FOUND, SUCCESS, UNKNOWN } from '../constants';
+const { VITE_API_URL } = import.meta.env;
 // eslint-disable-next-line react/prop-types
 export default function AuthContextProvider({ children }) {
+  const [phoneNumber, setPhoneNumber] = useState('');
   const navigate = useNavigate();
-  async function loginUser(user) {
+  async function loginUser(mobileNumber) {
     try {
-      const response = await axios.post(
-        `${VITE_API_URL}/login`,
-        user
-      );
+      const response = await axios.post(`${VITE_API_URL}/sendOtp`, mobileNumber);
       console.log(response);
       if (response.status === 200) {
-        if (response.data.token) {
-          window.localStorage.setItem("token", response.data.token);
-          navigate("/");
-          return SUCCESS;
-        }
+        // if (response.data.token) {
+        //   window.localStorage.setItem("token", response.data.token);
+        navigate('/verifyOtp');
+        return SUCCESS;
+        // }
+      }
+    } catch (error) {
+      if (error.response.status === 500) {
+        return UNKNOWN;
+      } else if (error.response.status === 404) {
+        return NOT_FOUND;
+      }
+    }
+  }
+  async function verifyOtp(mobileNumber, otp) {
+    const data = { mobileNumber, otp };
+    try {
+      const response = await axios.post(`${VITE_API_URL}/verifyOtp`, data);
+      console.log(response);
+      if (response.status === 200) {
+        // if (response.data.token) {
+        //   window.localStorage.setItem("token", response.data.token);
+        navigate('/');
+        return SUCCESS;
+        // }
       }
     } catch (error) {
       if (error.response.status === 500) {
@@ -31,11 +49,11 @@ export default function AuthContextProvider({ children }) {
   }
 
   function logoutUser() {
-    window.localStorage.removeItem("token");
-    navigate("/login");
+    window.localStorage.removeItem('token');
+    navigate('/login');
   }
   return (
-    <authContext.Provider value={{ loginUser, logoutUser }}>
+    <authContext.Provider value={{ loginUser, logoutUser, phoneNumber, setPhoneNumber, verifyOtp }}>
       {children}
     </authContext.Provider>
   );
