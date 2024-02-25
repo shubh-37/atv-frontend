8e86dd26429e6e36723e97a8496327016d0b5b51
Shubh Arya <shubh.arya37@gmail.com>
2024-02-25 16:21:07 +0530

    added new constants

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,16 @@
-const SUCCESS = "success";
-const NOT_FOUND = "not found";
-const UNKNOWN = "unknown";
-const BAD_REQUEST = "bad request";
-const NO_BARCODE = "no barcode";
-const PRODUCT_FOUND = "product found";
-const CREATE_PRODUCT = "new product";
-const NO_IMAGE = "no image";
-const AUTH_FAILED = "authentication error"
+const SUCCESS = 'success';
+const NOT_FOUND = 'not found';
+const UNKNOWN = 'unknown';
+const BAD_REQUEST = 'bad request';
+const NO_BARCODE = 'no barcode';
+const PRODUCT_FOUND = 'product found';
+const CREATE_PRODUCT = 'new product';
+const NO_IMAGE = 'no image';
+const AUTH_FAILED = 'authentication error';
+const SESSION_TIMED_OUT = 'session timed out';
+const INTERAKT_ERROR = "interakt issue";
+const MAX_RETRY_REACHED = "max otp retry reached";
+const OTP_SUCCESS = "otp sent successfully";
 
 export {
   SUCCESS,
@@ -17,5 +21,9 @@ export {
   PRODUCT_FOUND,
   CREATE_PRODUCT,
   NO_IMAGE,
-  AUTH_FAILED
+  AUTH_FAILED,
+  SESSION_TIMED_OUT,
+  INTERAKT_ERROR,
+  MAX_RETRY_REACHED,
+  OTP_SUCCESS
 };
